2774bd51b1907a2295d4bea409e42b6a67a30764
Maciej <maciejserafin2300@gmail.com>
2025-11-10 21:48:25 +0100

    poprawa na bardzej funkcyjna rzecz

--- app/src/Zadanie3.scala
+++ app/src/Zadanie3.scala
@@ -5,17 +5,27 @@ import app.Zadanie1.initialize
 object Zadanie3 extends cask.MainRoutes{
 
   @cask.postJson("/usuwanie")
-  def usuwanieEndpoint(data: ujson.Value): ujson.Value ={
+  def usuwanie(data: ujson.Value): ujson.Value = {
     val lista = data("List").arr.map(_.num.toInt).toList
-    val wynik = usuwanie(lista)
 
-    ujson.Obj("Wynik usuwania elemntu z listy" -> ujson.Arr(wynik.map(a => ujson.Num(a)).toSeq*))
+    val wynik: List[Int] = lista match {
+      case Seq() => Nil
+      case _ +: reszta => reszta.toList
+    }
 
-}
+
+    ujson.Obj("Wynik usuwania elemntu z listy" -> ujson.Arr(wynik.map(a => ujson.Num(a)).toSeq *))
+  }
+
+  /*
   def usuwanie[A](lista: Seq[A]): List[A] = lista match {
     case Seq() => Nil
     case _+: resztaListy => resztaListy.toList
-  }
+    }
+
+   */
+
+
 
   initialize()
 
