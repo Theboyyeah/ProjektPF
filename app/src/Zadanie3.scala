cde12e5097fe4a6e066fe3e20c9e97df64f8b94f
Maciej <maciejserafin2300@gmail.com>
2025-11-10 20:47:08 +0100

    Dodanie funkji usuwanie oraz jej logiki

--- app/src/Zadanie3.scala
+++ app/src/Zadanie3.scala
@@ -1,8 +1,16 @@
 package app
 
 object Zadanie3 {
-
+  @cask.postJson("/Delete")
+  def usunPierwszyElementListy[A](lista: Seq[A]): List[A] = lista match {
+    case Nil =>Nil
+    case
 
   }
+
+
+
+
+}
 
 }
