4849de0d3c75fd6c881b952b5919f92cb03fa711
Maciej <maciejserafin2300@gmail.com>
2026-01-06 16:04:13 +0100

    funcPromise dodane

--- Zadanie9Projekt/index.js
+++ Zadanie9Projekt/index.js
@@ -4,5 +4,25 @@ const app = express();
 
 app.use(express.json());
 
+function isPrime(liczba){
+    if (liczba % 2 === 0) return 0;
+    else return 1;
+
+    let  i = 3
+    const maxLimit = Math.sqrt(liczba)
+    while(i<maxLimit){
+        if(liczba % 1 ==0) return 0;
+        i+= 2;
+    }
+    return 1;
+
+
+}
+
+function funcPromise(liczba){
+    return Promise.resolve(isPrime(liczba))
+}
+
+
 
 
