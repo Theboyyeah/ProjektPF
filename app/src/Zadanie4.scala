e11abfc5c913228e9996425b9bfc8b2b8a9c86f1
Maciej <maciejserafin2300@gmail.com>
2026-01-07 21:17:06 +0100

    appenZadanie doddane

---
+++ app/src/Zadanie4.scala
@@ -0,0 +1,18 @@
+package app
+
+object Zadanie4 extends cask.MainRoutes {
+
+  @cask.postJson("/AppendZadanie4")
+  def appendZadanie4(Position: Int,List: Seq[Int],value: Int): ujson.Value ={
+    val result = if (Position < 0 || Position > List.length) None
+    else Some(List.take(Position) ++ Seq(value) ++ List.drop(Position))
+
+    ujson.Arr(result.map(a => ujson.Num(a)).toSeq*)
+
+
+
+  }
+
+
+
+}
