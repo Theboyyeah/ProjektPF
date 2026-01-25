bec435ac92bde11874b28557e663bfc91dfe805a
Maciej <maciejserafin2300@gmail.com>
2026-01-25 19:47:29 +0100

    poprawa

--- app/src/Zadanie5.scala
+++ app/src/Zadanie5.scala
@@ -1,14 +1,22 @@
 package app
 
-
+// bibliotka do zamiennia wygodniejszego obiektów sakli na json
+import upickle.default._
 
 object Zadanie5 extends cask.MainRoutes{
 
   @cask.postJson("/Mapa")
   def zliczLiczbyWliscie(lista: List[Int]) = {
-    val = lista
+    val listaRead = read[List[Int]](listaRead)
+
+    val result = lista.groupBy(identity).map { case (k, v) => k -> v.size }
+    write(result)
 
   }
+
+
+
+ initialize()
 
 
 
