8236b93077c6d219d0e045db4c9404a288c228c8
Maciej <maciejserafin2300@gmail.com>
2026-01-26 16:47:10 +0100

    main v2

--- Zestaw6/app/Main.hs
+++ Zestaw6/app/Main.hs
@@ -1,4 +1,51 @@
+import Web.Scotty
+
+
+import Data.Aeson
+import GHC.Generics
+
 module Main (main) where
+    
+
+setHead :: a -> [a] -> [a]
+setHead x xs = x : xs
+data Input = Input{
+    head :: Int , list :: [Int]
+
+}deriving (Show, Generic)
+
+
+data Output = Output {
+    newList :: [Int]
+
+}deriving (Show, Generic)
+
+instance FromJSON Input
+instance ToJSON Output
+
+setHead :: a -> [a] -> [a]
+setHead x xs = x : xs
+data Input = Input{
+    head :: Int , list :: [Int]
+
+}deriving (Show, Generic)
+
+
+instance FromJSON Input
+
+
+data Output = Output {
+    newList :: [Int]
+
+}deriving (Show, Generic)
+
+instance ToJSON Output
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = scotty 3000 $
+  get "/:word" $ do
+    input <- jsonData :: ActionM Input
+
+    let resultList = setHead (head input) (list input)
+
+    json (Output resultList)
