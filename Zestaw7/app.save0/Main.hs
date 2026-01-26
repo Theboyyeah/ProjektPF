562ad0267fe4e9db47389eb0410c3b4a87391713
Maciej <maciejserafin2300@gmail.com>
2026-01-26 23:35:56 +0100

    udało sie

---
+++ Zestaw7/app.save0/Main.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import Web.Scotty
+import Data.Aeson
+import GHC.Generics
+import System.Random (randomRIO)
+import Control.Monad.IO.Class (liftIO)
+
+
+randomValue :: IO Int
+randomValue  = randomRIO (0,maxBound :: Int)
+
+data Output = Output{
+
+    raondomNumber :: Int 
+
+} deriving (Show, Generic)
+
+instance ToJSON Output
+
+
+
+main :: IO ()
+main = scotty 3000 $
+  post "/RandomNumber" $ do
+    n <- liftIO randomValue
+    json $ Output n
+
+-- Jesli chodzi o to zodanie to jak rozumiem chodziło ,i np funckja ta moe zwracać randomową
+-- liczbe z podanego zakresu
+-- w zdaniach było powiedzina e mamy wysyłać jaki i odbierać w formacie JSON ale tutaj mona chyba poradzic sobie bez tego
+
