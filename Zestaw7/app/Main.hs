82565c802a3d3205aa84a7150efa9afaebdb6478
Maciej <maciejserafin2300@gmail.com>
2026-01-26 22:21:39 +0100

    do sprawdzenia

--- Zestaw7/app/Main.hs
+++ Zestaw7/app/Main.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
-
+-- Jesli chodzi o to zodanie to jak rozumiem chodziło ,i np funckja ta moe zwracać randomową
+-- liczbe z podanego zakresu
+-- w zdaniach było powiedzina e mamy wysyłać jaki i odbierać w formacie JSON ale tutaj mona chyba poradzic sobie bez tego
 
 module Main (main) where
 
@@ -11,11 +13,21 @@ import System.Random (randomRIO)
 
 
 randomValue :: IO Int
-randomValue randomRIO (0,maxBount,Int)
+randomValue  = randomRIO (0,maxBount :: Int)
+
+data Output = Output{
+
+    raondomNumber :: Int 
+
+} deriving (Show, Generic)
+
+instance ToJSON Output
 
 
 
 main :: IO ()
 main = scotty 3000 $
   post "/setHead" $ do
+    n <- liftIO randomValue
+     json $ Output n
 
